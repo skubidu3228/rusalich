b87f78bea959e8a526b753c07142579fc0c784be
Nikita <skubidu32281@gmail.com>
2020-03-16 21:14:43 +0200

    HW for Testing Reddit

--- src/test/java/second.java
+++ src/test/java/second.java
@@ -1,29 +1,41 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;;
-import org.openqa.selenium.Keys;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.Assert.assertEquals;
 
-public class first {
+public class second {
     private final By btnSearch = By.xpath("//a[contains(@class, 'search')]");
     private final By inpSearchBox = By.xpath("//input[@type='text']");
     private final By searchResult = By.xpath("//div[@class='search-page__list']//h3");
     private final String url = "https://www.softserveinc.com/uk-ua";
     private final String query = "academy";
     private final String expectedResult = "IT Академія";
+    static WebDriver driver;
     @Test public void FirstTest(){
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
+        WebDriverWait webDriverWait = new WebDriverWait(driver, 2);
         driver.manage().window().maximize();
         driver.navigate().to(url);
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         driver.findElement(btnSearch).click();
+        webDriverWait.until(d -> driver.findElement(inpSearchBox).isDisplayed());
         driver.findElement(inpSearchBox).sendKeys(query);
         driver.findElement(inpSearchBox).sendKeys(Keys.ENTER);
-        Assert.assertEquals(driver.findElements(searchResult).get(0).getText(), expectedResult);
+        assertEquals(driver.findElements(searchResult).get(0).getText(), expectedResult);
+        Assert.assertTrue(driver.getCurrentUrl().contains("softserve"));
 //        driver.manage().window().maximize();
 //        driver.get("https://www.google.com/");
 //        driver.findElement(By.name("q")).sendKeys("SoftServe");
@@ -35,7 +47,20 @@ public class first {
 //        else{
 //            System.out.println("First link - not SoftServe, it's False");
 //        }
-         driver.quit();
+        driver.quit();
 
+    }
+    @AfterMethod
+    public void takeScreenshot(ITestResult result) {
+        if (!result.isSuccess()) {
+            try {
+                TakesScreenshot screenshot = (TakesScreenshot) driver;
+                File source = screenshot.getScreenshotAs(OutputType.FILE);
+                FileUtils.copyFile(source, new File("Screenshots\\"
+                        + result.getName() + "[" + LocalDate.now() + "][" + System.currentTimeMillis() + "].png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
